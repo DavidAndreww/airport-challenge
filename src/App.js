d7bd14ab46cdbdc029b41476b53bd073b20c70ea
unknown <d_bille@yahoo.com>
2020-09-22 10:06:01 -0500

    set up router to switch between user view and admin panel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
 import NavigationModel from './components/navigation/NavigationModel'
-import DisplayField from './components/DisplayField'
 import FooterModel from './components/footer/FooterModel'
-
+import Router from './Router'
 function App () {
   return (
     <div className='form' style={{ maxWidth: '550px', margin: '0 auto' }}>
       <Provider store={store}>
         <NavigationModel />
-        <DisplayField />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <FooterModel />
       </Provider>
     </div>
--- src/Router.js
+++ src/Router.js
@@ -1,2 +1,13 @@
 import React from 'react'
-import { Switch, Route } from 
+import { Switch, Route } from 'react-router'
+import AdminView from '../src/components/adminPanel/adminView'
+import DisplayField from '../src/components/DisplayField'
+
+export default function Router () {
+  return (
+    <Switch>
+      <Route exact path='/' component={DisplayField} />
+      <Route path='/admin' component={AdminView} />
+    </Switch>
+  )
+}
