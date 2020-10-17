d132d316c195bd26ad46bb625e7fd5c3a0a8c8d1
unknown <d_bille@yahoo.com>
2020-10-16 21:32:59 -0500

    trying to refactor recursive function

--- server/controllers/postData.js
+++ server/controllers/postData.js
@@ -10,26 +10,47 @@ function doesFileExist (path) {
   }
 }
 
+// module.exports = function postData (req, res) {
+//   const json = req.body
+//   const path = './server/data/data.json'
+
+//   if (doesFileExist(path)) {
+//     fs.readFile(path, 'utf8', function (err, data) {
+//       if (err) throw err
+//       const arrOfData = JSON.parse(data)
+//       arrOfData.data.push(json)
+
+//       fs.writeFile(path, JSON.stringify(arrOfData), 'utf8', function (err) {
+//         if (err) throw err
+//         res.json({ new_entry: json })
+//       })
+//     })
+//   } else {
+//     const jsonStarterData = { data: [] }
+//     fs.writeFile(path, JSON.stringify(jsonStarterData), 'utf8', (err) => {
+//       if (err) throw err
+//       postData(req, res)
+//     })
+//   }
+// }
+
 module.exports = function postData (req, res) {
   const json = req.body
   const path = './server/data/data.json'
 
+  let currentEntries = []
+
   if (doesFileExist(path)) {
     fs.readFile(path, 'utf8', function (err, data) {
       if (err) throw err
-      const arrOfData = JSON.parse(data)
-      arrOfData.data.push(json)
-
-      fs.writeFile(path, JSON.stringify(arrOfData), 'utf8', function (err) {
-        if (err) throw err
-        res.json({ new_entry: json })
-      })
-    })
-  } else {
-    const jsonStarterData = { data: [] }
-    fs.writeFile(path, JSON.stringify(jsonStarterData), 'utf8', (err) => {
-      if (err) throw err
-      postData(req, res)
+      currentEntries = JSON.parse(data).data
     })
   }
+  const newEntry = req.body
+  currentEntries.push(newEntry)
+
+  fs.writeFile(path, { data: currentEntries }, 'utf8', function (err) {
+    if (err) throw err
+    res.json({ new_entry: newEntry })
+  })
 }
